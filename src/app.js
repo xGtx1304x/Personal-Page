46777f6578bafc36da5389d7b10231144dec097d
xGtx1304x <jorgedc1304@gmail.com>
2025-08-27 22:54:47 -0600

    Nav hides when option is selected added

--- src/app.js
+++ src/app.js
@@ -48,21 +48,27 @@ menuToggle.addEventListener("click", () => {
 });
 
 overlay.addEventListener("click", () => {
-    nav.classList.remove("active");
-    overlay.classList.remove("active");
-  });
+  nav.classList.remove("active");
+  overlay.classList.remove("active");
+});
 
 homebtn.addEventListener("click", () => {
   title.innerText = text[0][0];
   content.innerText = text[0][1];
+  nav.classList.remove("active");
+  overlay.classList.remove("active");
 });
 
 aboutbtn.addEventListener("click", () => {
   title.innerText = text[1][0];
   content.innerText = text[1][1];
+  nav.classList.remove("active");
+  overlay.classList.remove("active");
 });
 
 contactbtn.addEventListener("click", () => {
   title.innerText = text[2][0];
   content.innerText = text[2][1];
+  nav.classList.remove("active");
+  overlay.classList.remove("active");
 });
