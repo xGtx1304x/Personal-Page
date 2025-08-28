a886e414b3d9661fdfc62b7cd928daf9994a7b51
xGtx1304x <jorgedc1304@gmail.com>
2025-08-28 16:57:51 -0600

    Header updated

--- src/header.js
+++ src/header.js
@@ -1,13 +1,15 @@
 function Header() {
   return `
     <header>
-  <h1>My Page</h1>
+  <a id="LogoHome">
+    <img src="src/assets/logo header.png" alt="Logo">
+  </a>
   <button class="menu-toggle">☰</button>
   <nav>
     <ul>
-      <li><button id="Home" id="Home">Home</button></li>
       <li><button id="About" id="About">Cursos</button></li>
       <li><button id="Contact" id="Contact">Sobre mí</button></li>
+      <li><button id="Home" id="Home">Home</button></li>
     </ul>
   </nav>
 </header>
